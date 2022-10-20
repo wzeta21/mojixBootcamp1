f2573581824213beb0bbae9c4b41844c031f1f9c
Wilder Zapata Miranda <wzeta21@gmail.com>
2022-10-20 19:31:07 -0400

    adding from intelliJ

---
+++ trainingBootcamp/src/main/java/Client.java
@@ -0,0 +1,13 @@
+public class Client {
+    public static void main(String arg[]){
+        int a = 9;
+        int b = 8;
+        if (a > b){
+            a = a * 2;
+            System.out.println(a);
+        } else {
+            b = b - 2;
+            System.out.println(a);
+        }
+    }
+}
